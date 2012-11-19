69ae5f3ea7c2dbd780e992c8ec1bc142efb055bc
Joacim Nybergh <joacim@nybergh.se>
2012-11-19 17:27:37 +0100

    Changed 'Sample App' to 'TwitterHuck'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to the TwitterHuck!"
       redirect_to @user
     else
       render 'new'
